47ec214981c4b47677885242c0510359cf7711f0
htactive-nhanhdt <nhanhdt@htactive.com>
2019-12-06 14:29:09 +0700

    229

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,17 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [        
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
