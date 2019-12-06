bdd98496e3b210ef25417c121aa2f998feed018a
htactive-nhanhdt <nhanhdt@htactive.com>
2019-12-06 13:52:41 +0700

    152

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
@@ -15,9 +8,7 @@ const SEO = ({
   description,
   lang,
   meta,
-  title,
-  active,
-  data,
+  title,  
   changeColorRedux,
   changeFooterRedux,
   changeLayoutRedux,
